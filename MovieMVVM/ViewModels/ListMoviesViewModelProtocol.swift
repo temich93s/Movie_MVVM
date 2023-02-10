3718ce06771cefc70e9018e6e4d459e079079ff5
Temich93s <nil>
2023-02-10 10:39:38 +1000

    Сделал часть CoreData и Keychain

--- MovieMVVM/ViewModels/ListMoviesViewModelProtocol.swift
+++ MovieMVVM/ViewModels/ListMoviesViewModelProtocol.swift
@@ -5,13 +5,9 @@ import Foundation
 
 /// Протокол вью-модель экрана со списком фильмов
 protocol ListMoviesViewModelProtocol {
-    // MARK: - Public Properties
-
     var currentCategoryMovies: CategoryMovies { get set }
     var listMoviesState: ((ListMoviesState<Movie>) -> ())? { get set }
     var uploadApiKeyCompletion: VoidHandler? { get set }
-
-    // MARK: - Public Methods
 
     func fetchMovies()
     func fetchData(movie: Movie, completion: @escaping ((Result<Data, Error>) -> Void))
