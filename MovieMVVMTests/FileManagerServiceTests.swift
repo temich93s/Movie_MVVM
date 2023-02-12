876d296cd3a71008ccbdc7215c6080c1ad841361
Temich93s <nil>
2023-02-12 10:09:15 +1000

    Добавил Unit и UI тесты

---
+++ MovieMVVMTests/FileManagerServiceTests.swift
@@ -0,0 +1,40 @@
+// FileManagerServiceTests.swift
+// Copyright © SolovevAA. All rights reserved.
+
+import XCTest
+
+@testable import MovieMVVM
+
+/// Тестирование файлового сервиса
+final class FileManagerServiceTests: XCTestCase {
+    // MARK: - Private Constants
+
+    private enum Constants {
+        static let mockPencilText = "pencil"
+        static let mockNumber = 8
+    }
+
+    // MARK: - Private Properties
+
+    let mockProxy = MockProxy()
+    let mockPath = Constants.mockPencilText
+
+    var fileManagerService: FileManagerService?
+
+    // MARK: - Public Methods
+
+    override func setUp() {
+        fileManagerService = FileManagerService()
+    }
+
+    override func tearDown() {
+        fileManagerService = nil
+    }
+
+    func testLoadImage() {
+        let mockData = Data(count: Constants.mockNumber)
+        fileManagerService?.saveImageData(path: Constants.mockPencilText, data: mockData)
+        let data = fileManagerService?.loadImageData(path: Constants.mockPencilText)
+        XCTAssertEqual(mockData, data)
+    }
+}
